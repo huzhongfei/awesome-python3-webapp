30e356848e4aa23263a1ba54b3b9944ab4376732
hu7fei2 <hu7fei2@sina.com>
2019-11-14 19:57:30 +0800

    11.14 第1、2天及第3天部分内容

---
+++ www/orm.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import asyncio
+import logging
+
+import aiomysql
+
+logging.basicConfig(level=logging.INFO)
+
+
+def log(sql, args=()):
+    logging.info('SQL: %s' % sql)
+
+
+async def create_pool(loop, **kw):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = await aiomysql.create_pool(
+        kw.get('minseze',1), # 连接池大小
+        kw.get('maxsize', 10),
+        loop=loop,
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset', 'utf8'),
+        autocommit=kw.get('autocommit', True)
+    )
+
+
+# select语句
+async def select(sql, args, size=None):
+    log(sql, args)
+    global __pool
+    with (await __pool) as conn:
+        cur = await conn.cursor(aiomysql.DictCursor) # 游标结果作为字典返回
+        await cur.execute(sql.replace('?', ' %s'), args or ())  # c查询 , 返回 受影响的异常声的行数
+        if size:
+            rs = await cur.fetchmany(size)
+        else:
+            rs = await cur.fetchall()
+        await cur.close()
+        logging.info('rows returned: %s' % len(rs))
+        return rs
+
+
+# insert update delete
+async def execute(sql, args):
+    log(sql)
+    with(await __pool) as conn:
+        try:
+            cur = await conn.cursor()
+            await cur.execute(sql.replace('?', '%s'), args)
+            affected = cur.rowcount
+            await cur.close()
+        except BaseException as e:
+            raise
+        return affected
+
+
+class ModelMetaclass(type):
+    pass
+
+
+class Model(dict, metaclass=ModelMetaclass):
+
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getValue(self, key):
+        return getattr(self, key, None)
+
+    def getValueOrDefault(self, key):
+        value = getattr(self, key, None)
+        if value is None:
+            field = self.__mappings__[key]
+            if field.default is not None:
+                value = field.default() if callable(field.default) else field.default
+                logging.debug("using default value for %s: %s" % (key, str(value)))
+                setattr(self, key, value)
+            return value
+
+
+class Field(object):
+    def __init__(self, name, column_type, primary_key, default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+
+    def __str__(self):
+        return '<%s, %s, %s>' % (self.__class__.name, self.column_type, self.name)
+
+
+# 映射 varchar 的 StringField
+class StringField(Field):
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
