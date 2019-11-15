bead5e2d8cb0c71c59931a28781ac34ce0183da5
hu7fei2 <hu7fei2@sina.com>
2019-11-15 13:45:37 +0800

    11.15 第1、2天及第3天部分内容

--- www/orm.py
+++ www/orm.py
@@ -59,8 +59,53 @@ async def execute(sql, args):
         return affected
 
 
+def create_args_string(num):
+    L = []
+    for n in range(num):
+        L.append('?')
+    return ', '.join(L)
+
+
+# Model 基类， 将具体的子类映射信息读取出来，通过ModelMetaclass
 class ModelMetaclass(type):
-    pass
+    def __new__(cls, name, bases, attrs):
+        # 排除Model类本身
+        if name=='Model':
+            return type.__new__(cls,name, bases, attrs)
+        # 获取 table 名称:
+        tableName = attrs.get('__table__', None) or name
+        logging.info('found model: %s (table: %s)' % (name, tableName))
+        # 获取所有的Field和主键名
+        mappings = dict()
+        fields = []
+        primaryKey = None
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                logging.info('  found mapping: %s ==> %s' % (k, v))
+                mappings[k] = v
+                if v.parmary_key:
+                    # 找到主键
+                    if primaryKey:
+                        raise RuntimeError('Duplicate primary key for field: %s' % k)
+                    primaryKey = k
+                else:
+                    fields.append(k)
+        if not primaryKey:
+            raise RuntimeError('Primary key not found.')
+        for k in mappings.keys():
+            attrs.pop(k)
+        escaped_fields = list(map(lambda f:  '`%s`' % f, fields))
+        attrs['__mappings__'] = mappings # 保存属性和列的映射关系
+        attrs['__table__'] = tableName
+        attrs['__primary_key__'] = primaryKey # 主键属性名
+        attrs['__fields__'] = fields # 除主键外的属性名
+        # 构造默认的SELECTE, INSERT, UPDATE 和 DELETE语句:
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into  `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+        return type.__new__(cls, name, bases, attrs)
+
 
 
 class Model(dict, metaclass=ModelMetaclass):
@@ -89,6 +134,14 @@ class Model(dict, metaclass=ModelMetaclass):
                 logging.debug("using default value for %s: %s" % (key, str(value)))
                 setattr(self, key, value)
             return value
+
+    @classmethod
+    async def find(cls, pk):
+        ' find object by primary key. '
+        rs = await select('%s where `%s`=?' % (cls.__selcect__, cls.__primary_key__), [pk], 1)
+        if len(rs) == 0:
+            return None
+        return cls(**rs[0])
 
 
 class Field(object):
